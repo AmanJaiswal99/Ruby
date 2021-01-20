243a81eb0e170bfa0ead20cb5f09b3dd5e0d2657
AmanJaiswal99 <51396136+AmanJaiswal99@users.noreply.github.com>
2021-01-20 12:23:43 +0530

    first

---
+++ ruby tutorial/intro.rb
@@ -0,0 +1,9 @@
+puts "hello world" #prints on new line
+print "hello world" 
+print " this is ruby" #will print on the same line as the above print statement
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"        #in ruby code is executed line by line
+
